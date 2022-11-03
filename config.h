e36b649840e6add3d4202e758f5035a9a3cb298b
EllieT7 <elizabeth.tacachira@gmail.com>
2022-11-03 08:56:56 -0400

    First commit

---
+++ config.h
@@ -0,0 +1,4 @@
+//const char* ssid = "XXXXXXXXXXXXXXXXXX";        //Indicamos el nombre de la red WiFi (SSID) a la que queremos conectarnos.
+//const char* password =  "XXXXXXXXXXXXXXXXXX";    //Indicamos la contraseña de de red WiFi
+const char* ssid = "anonimo";
+const char* password =  "123456789y"; 
